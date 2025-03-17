a0f326ff6a03fadda08a8052735e7f171f5ccad5
robson arruda <robsonshockwavegithub@gmail.com>
2025-03-16 21:33:59 -0300

    cria useCase buscarUsuarioPorCPF

---
+++ src/buscar-usuario-por-cpf.usecase.js
@@ -0,0 +1,9 @@
+const { Either } = require('./shared/errors');
+
+module.exports = function buscarUsuarioPorCPFUseCase({ usuariosRepository }) {
+  return async function ({ CPF }) {
+    const usuario = await usuariosRepository.buscarPorCPF(CPF);
+
+    return Either.Right(usuario);
+  };
+};
