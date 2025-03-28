b61b9811f089abd1c0102b4b3de0dda8e13f7223
robson arruda <robsonshockwavegithub@gmail.com>
2025-03-28 10:30:25 -0300

    deve retornar um error se os campos obrigatórios não forem passados para o emprestimos entity

--- src/enterprise/entities/emprestimo.entity.js
+++ src/enterprise/entities/emprestimo.entity.js
@@ -1,3 +1,5 @@
+const { AppError } = require('../../shared/errors');
+
 const emprestimosEntity = function () {
   function calcularDiasAtraso({ data_retorno, data_devolucao }) {
     return (
@@ -6,6 +8,9 @@ const emprestimosEntity = function () {
   }
 
   const calcularMulta = function ({ data_retorno, data_devolucao }) {
+    if (!data_retorno || !data_devolucao)
+      throw new AppError(AppError.parametrosObrigatoriosAusentes);
+
     const diasAtraso = calcularDiasAtraso({ data_retorno, data_devolucao });
 
     return `Multa por atraso: R$ ${diasAtraso > 0 ? '10,00' : '0'}`;
