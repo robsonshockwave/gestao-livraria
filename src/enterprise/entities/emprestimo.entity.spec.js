81e563cd1e7ee27e7c3ac15a22cd83baf4045b96
robson arruda <robsonshockwavegithub@gmail.com>
2025-03-28 10:26:22 -0300

    cria o teste de retorno sem multa na entity emprestimos

---
+++ src/enterprise/entities/emprestimo.entity.spec.js
@@ -0,0 +1,12 @@
+const emprestimoEntity = require('./emprestimo.entity');
+
+describe('Emprestimos Entity', function () {
+  test('calcularMulta - sem atraso', function () {
+    const resultado = emprestimoEntity.calcularMulta({
+      data_devolucao: '2024-01-01',
+      data_retorno: '2024-01-01',
+    });
+
+    expect(resultado).toBe('Multa por atraso: R$ 0');
+  });
+});
