8afcdb7670fbcedeab906bea487d93ec36ea81d4
robson arruda <robsonshockwavegithub@gmail.com>
2025-03-28 11:02:46 -0300

    cria o buscar emprestimos pendentes useCase

---
+++ src/application/buscar-emprestimos.usecase.js
@@ -0,0 +1,14 @@
+const { Either, AppError } = require('../shared/errors');
+
+module.exports = function buscarEmprestimosPendentesUseCase({
+  emprestimosRepository,
+}) {
+  if (!emprestimosRepository) throw new AppError(AppError.dependencias);
+
+  return async function () {
+    const emprestimosPendentes =
+      await emprestimosRepository.buscarPendentesComLivroComUsuario();
+
+    return Either.Right(emprestimosPendentes);
+  };
+};
