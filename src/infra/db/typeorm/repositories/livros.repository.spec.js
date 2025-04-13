0903f6275fe597ae96db74edfe61cf0de4f7359f
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-12 22:21:22 -0300

    cria o repositorio de livros e o metodo de cadastrar

---
+++ src/infra/db/typeorm/repositories/livros.repository.spec.js
@@ -0,0 +1,23 @@
+const { livrosRepository } = require('./livros.repository');
+
+describe('Livros Repository Typeorm', function () {
+  let sut;
+
+  beforeAll(function () {
+    sut = livrosRepository();
+  });
+
+  const livroDTO = {
+    nome: 'nome_valido',
+    quantidade: 'quantidade_valida',
+    autor: 'autor_valido',
+    genero: 'genero_valido',
+    ISBN: 'ISBN_valido',
+  };
+
+  test('Deve retornar void ao criar um livro', async function () {
+    const livroCriado = await sut.cadastrar(livroDTO);
+
+    expect(livroCriado).toBeUndefined();
+  });
+});
