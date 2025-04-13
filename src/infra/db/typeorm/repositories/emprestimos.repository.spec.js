62b5e8a452d7b73baba843f5387702b80871eedf
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-13 10:41:17 -0300

    cria o metodo de devolver

--- src/infra/db/typeorm/repositories/emprestimos.repository.spec.js
+++ src/infra/db/typeorm/repositories/emprestimos.repository.spec.js
@@ -1,4 +1,7 @@
-const { emprestimosRepository } = require('./emprestimos.repository');
+const {
+  emprestimosRepository,
+  typeormEmprestimosRepository,
+} = require('./emprestimos.repository');
 const { typeormUsuariosRepository } = require('./usuarios.repository');
 const { typeormLivrosRepository } = require('./livros.repository');
 
@@ -9,22 +12,25 @@ describe('Emprestimos Repository Typeorm', function () {
     sut = emprestimosRepository();
   });
 
-  test('Deve retornar void ao criar um empréstimo', async function () {
-    const usuario = await typeormUsuariosRepository.save({
-      nome_completo: 'qualquer_nome',
-      CPF: 'qualquer_CPF',
-      telefone: 'qualquer_telefone',
-      email: 'qualquer_email',
-      endereco: 'qualquer_endereco',
-    });
+  const usuarioDTO = {
+    nome_completo: 'qualquer_nome',
+    CPF: 'qualquer_CPF',
+    telefone: 'qualquer_telefone',
+    email: 'qualquer_email',
+    endereco: 'qualquer_endereco',
+  };
 
-    const livro = await typeormLivrosRepository.save({
-      nome: 'qualquer_nome',
-      quantidade: 3,
-      autor: 'qualquer_autor',
-      genero: 'qualquer_genero',
-      ISBN: 'qualquer_ISBN',
-    });
+  const livroDTO = {
+    nome: 'qualquer_nome',
+    quantidade: 3,
+    autor: 'qualquer_autor',
+    genero: 'qualquer_genero',
+    ISBN: 'qualquer_ISBN',
+  };
+
+  test('Deve retornar void ao criar um empréstimo', async function () {
+    const usuario = await typeormUsuariosRepository.save(usuarioDTO);
+    const livro = await typeormLivrosRepository.save(livroDTO);
 
     const emprestimoCriado = await sut.emprestar({
       usuario_id: usuario.id,
@@ -34,5 +40,24 @@ describe('Emprestimos Repository Typeorm', function () {
     });
 
     expect(emprestimoCriado).toBeUndefined();
+  });
+
+  test('Deve retornar a data de retorno salva no banco de dados corretamente', async function () {
+    const usuario = await typeormUsuariosRepository.save(usuarioDTO);
+    const livro = await typeormLivrosRepository.save(livroDTO);
+
+    const emprestimo = await typeormEmprestimosRepository.save({
+      usuario_id: usuario.id,
+      livro_id: livro.id,
+      data_saida: '2025-01-02',
+      data_retorno: '2025-01-02',
+    });
+
+    const devolver = await sut.devolver({
+      emprestimo_id: emprestimo.id,
+      data_devolucao: '2025-01-02',
+    });
+
+    expect(devolver.data_retorno).toBe(emprestimo.data_retorno);
   });
 });
