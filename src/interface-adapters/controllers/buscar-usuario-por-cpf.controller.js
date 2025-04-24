016d42654a78925496134178bdf19984ae37aff3
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-24 20:46:33 -0300

    cria o teste de buscar usuario por CPF com sucesso

---
+++ src/interface-adapters/controllers/buscar-usuario-por-cpf.controller.js
@@ -0,0 +1,27 @@
+const { z } = require('zod');
+const httpResponse = require('../../shared/helpers/http.response');
+
+const zodValidator = z.object({
+  CPF: z
+    .string({
+      required_error: 'CPF é obrigatório',
+    })
+    .refine((value) => {
+      const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+      return regex.test(value);
+    }),
+});
+
+module.exports = async function buscarUsuarioPorCPFController({
+  buscarUsuarioPorCPFUseCase,
+  httpRequest,
+}) {
+  const { CPF } = zodValidator.parse(httpRequest.params);
+
+  const output = await buscarUsuarioPorCPFUseCase({ CPF });
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (usuario) => httpResponse(200, usuario)
+  );
+};
