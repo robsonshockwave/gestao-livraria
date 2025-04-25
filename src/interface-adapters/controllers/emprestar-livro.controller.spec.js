05edfa4e8f12f8d5577b71050bd899fe86459b1b
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-24 22:02:44 -0300

    cria o teste de sucesso que retorna um httpResponse 201 e null ao emprestar um livro

---
+++ src/interface-adapters/controllers/emprestar-livro.controller.spec.js
@@ -0,0 +1,34 @@
+const { Either } = require('../../shared/errors');
+const httpResponse = require('../../shared/helpers/http.response');
+const emprestarLivroController = require('./emprestar-livro.controller');
+
+describe('Emprestar livro Controller', function () {
+  const emprestarLivroUseCase = jest.fn();
+
+  test('Deve retornar um httpResponse 201 e null se o livro for emprestado com sucesso', async function () {
+    emprestarLivroUseCase.mockResolvedValue(Either.Right(null));
+
+    const httpRequest = {
+      body: {
+        usuario_id: 1,
+        livro_id: 1,
+        data_saida: '2025-01-02',
+        data_retorno: '2025-01-04',
+      },
+    };
+
+    const response = await emprestarLivroController({
+      emprestarLivroUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(201, null));
+    expect(emprestarLivroUseCase).toHaveBeenCalledWith({
+      usuario_id: 1,
+      livro_id: 1,
+      data_saida: expect.any(Date),
+      data_retorno: expect.any(Date),
+    });
+    expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
+  });
+});
