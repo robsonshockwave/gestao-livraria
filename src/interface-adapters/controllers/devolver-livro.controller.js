d5fff24774b705cdbae82519a0ac1a5a2fa23535
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-24 22:22:40 -0300

    cria o controller devolverLivroController

---
+++ src/interface-adapters/controllers/devolver-livro.controller.js
@@ -0,0 +1,35 @@
+const { z } = require('zod');
+const httpResponse = require('../../shared/helpers/http.response');
+
+const zodValidatorBody = z.object({
+  data_devolucao: z.string({
+    required_error: 'Data devolução é obrigatório',
+  }),
+});
+
+module.exports = async function devolverLivroController({
+  devolverLivroUseCase,
+  httpRequest,
+}) {
+  if (
+    !devolverLivroUseCase ||
+    !httpRequest ||
+    !httpRequest.body ||
+    !httpRequest.params
+  ) {
+    throw new AppError(AppError.dependencias);
+  }
+
+  const { data_devolucao } = zodValidatorBody.parse(httpRequest.body);
+  const { emprestimo_id } = httpRequest.params;
+
+  const output = await devolverLivroUseCase({
+    emprestimo_id,
+    data_devolucao: new Date(data_devolucao),
+  });
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (resultado) => httpResponse(200, resultado)
+  );
+};
