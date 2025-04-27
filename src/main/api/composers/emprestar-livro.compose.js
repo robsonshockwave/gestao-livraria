1946d8d9ad213ca98aa6086d43d0a81bea3652df
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-26 23:50:38 -0300

    cria a rota de emprestar livro e o compose

---
+++ src/main/api/composers/emprestar-livro.compose.js
@@ -0,0 +1,18 @@
+const emprestarLivroUseCase = require('../../../application/emprestar-livro.usecase');
+const {
+  emprestimosRepository,
+} = require('../../../infra/db/typeorm/repositories/emprestimos.repository');
+const emprestarLivroController = require('../../../interface-adapters/controllers/emprestar-livro.controller');
+
+module.exports = async function emprestarLivroCompose(httpRequest) {
+  const emprestimosRepositoryFn = emprestimosRepository();
+  const emprestarLivroUseCaseFn = emprestarLivroUseCase({
+    emprestimosRepository: emprestimosRepositoryFn,
+  });
+  const controller = await emprestarLivroController({
+    emprestarLivroUseCase: emprestarLivroUseCaseFn,
+    httpRequest,
+  });
+
+  return controller;
+};
--- src/main/api/routes/emprestimos.routes.js
+++ src/main/api/routes/emprestimos.routes.js
@@ -1,5 +1,16 @@
 const { Router } = require('express');
+const emprestarLivroCompose = require('../composers/emprestar-livro.compose');
 
 const emprestimosRoutes = Router();
+
+emprestimosRoutes.post('/', async (req, res) => {
+  const httpRequest = {
+    body: req.body,
+  };
+
+  const { statusCode, body } = await emprestarLivroCompose(httpRequest);
+
+  return res.status(statusCode).json(body);
+});
 
 module.exports = { emprestimosRoutes };
