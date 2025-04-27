ecdce0843e5411c587713fdbfcf4df6e740dec8a
robson arruda <robsonshockwavegithub@gmail.com>
2025-04-26 22:10:13 -0300

    cria o cadastrar livro compose e a rota de cadastrar livro

--- src/main/api/routes/livros.routes.js
+++ src/main/api/routes/livros.routes.js
@@ -1,7 +1,16 @@
 const { Router } = require('express');
+const cadastrarLivroCompose = require('../composers/cadastrar-livro.compose');
 
 const livrosRoutes = Router();
 
-livrosRoutes.post('/', (req, res) => {});
+livrosRoutes.post('/', (req, res) => {
+  const httpRequest = {
+    body: req.body,
+  };
+
+  const { statusCode, body } = cadastrarLivroCompose(httpRequest);
+
+  return res.status(statusCode).json(body);
+});
 
 module.exports = { livrosRoutes };
